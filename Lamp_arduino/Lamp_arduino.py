91a7218fe0063a9feb1a748ac31aa28b38052472
Tanya <vorobest5@gmail.com>
2023-08-31 02:44:31 +0300

    list bluetooth device in comboBox

--- Lamp_arduino/Lamp_arduino.py
+++ Lamp_arduino/Lamp_arduino.py
@@ -1,30 +1,39 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtBluetooth import QBluetoothDeviceInfo, QBluetoothDeviceDiscoveryAgent, QBluetoothLocalDevice
-
-from PyQt5.QtCore import QIODevice, pyqtSlot, QCoreApplication
+from PyQt5.QtCore import QIODevice, pyqtSlot, QCoreApplication, QTimer, QEventLoop
 
 app = QtWidgets.QApplication([])
 ui = uic.loadUi("des.ui")
 ui.setWindowTitle("Test_Lamp")
 
+list_device = []
 
 def startDeviceDiscovery():
-    app = QCoreApplication([])
-
     # Create a discovery agent and connect to its signals
     discoveryAgent = QBluetoothDeviceDiscoveryAgent()
-    discoveryAgent.deviceDiscovered.connect(deviceDiscovered)
+    discoveryAgent.deviceDiscovered.connect(deviceDiscover)
 
     # Start a discovery
     discoveryAgent.start()
 
-    app.exec_()
+    # Create an event loop to manage the execution
+    loop = QEventLoop()
 
-@pyqtSlot(QBluetoothDeviceInfo)
-def deviceDiscovered(device):
+    # Set up a timer to exit the event loop after a certain time
+    timer = QTimer()
+    timer.timeout.connect(loop.quit)
+    timer.start(3000)  # 10 seconds
+
+    loop.exec_()
+def deviceDiscover(device):
     print(f"Found device: {device.name()} ({device.address().toString()})")
+    find_device=device.name()+" "+device.address().toString()
+    list_device.append(find_device)
 
 startDeviceDiscovery()
+print(list_device)
+ui.BluetoothBox.addItems(list_device)
 
 ui.show()
 app.exec()
+
