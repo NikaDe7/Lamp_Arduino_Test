8e0842c68ba2f485e88c6c4b85a663bbb37e10ff
Tanya <vorobest5@gmail.com>
2023-08-30 20:20:04 +0300

    bluetooth list

--- Lamp_arduino/Lamp_arduino.py
+++ Lamp_arduino/Lamp_arduino.py
@@ -1,10 +1,30 @@
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtBluetooth import QBluetoothDeviceInfo, QBluetoothDeviceDiscoveryAgent, QBluetoothLocalDevice
 
-from PyQt5.QtCore import QIODevice
+from PyQt5.QtCore import QIODevice, pyqtSlot, QCoreApplication
 
 app = QtWidgets.QApplication([])
 ui = uic.loadUi("des.ui")
 ui.setWindowTitle("Test_Lamp")
+
+
+def startDeviceDiscovery():
+    app = QCoreApplication([])
+
+    # Create a discovery agent and connect to its signals
+    discoveryAgent = QBluetoothDeviceDiscoveryAgent()
+    discoveryAgent.deviceDiscovered.connect(deviceDiscovered)
+
+    # Start a discovery
+    discoveryAgent.start()
+
+    app.exec_()
+
+@pyqtSlot(QBluetoothDeviceInfo)
+def deviceDiscovered(device):
+    print(f"Found device: {device.name()} ({device.address().toString()})")
+
+startDeviceDiscovery()
 
 ui.show()
 app.exec()
