3b9092cca737a2504293985ddcb005352152761f
Tanya <vorobest5@gmail.com>
2023-08-30 16:42:14 +0300

    Початок роботи, підключення дизайну

---
+++ Lamp_arduino/Lamp_arduino.py
@@ -0,0 +1,7 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+from PyQt5.QtCore import QIODevice
+
+app = QtWidgets.QApplication([])
+ui = uic.loadUi("des.ui")
+ui.setWindowTitle("SerialGui")
