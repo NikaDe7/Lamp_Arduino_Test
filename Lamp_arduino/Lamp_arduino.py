81a42368231768fc674ee424e7a6476f190d8176
Tanya <vorobest5@gmail.com>
2023-08-30 17:01:45 +0300

    first

--- Lamp_arduino/Lamp_arduino.py
+++ Lamp_arduino/Lamp_arduino.py
@@ -1,7 +1,8 @@
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
+
 from PyQt5.QtCore import QIODevice
 
 app = QtWidgets.QApplication([])
 ui = uic.loadUi("des.ui")
 ui.setWindowTitle("SerialGui")
+
