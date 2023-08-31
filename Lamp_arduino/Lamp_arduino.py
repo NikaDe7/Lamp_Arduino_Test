9f7d8007bd2ff819f7f8080ed82e0f61cd7812ce
Tanya <vorobest5@gmail.com>
2023-09-01 01:55:15 +0300

    connect to HC-06

--- Lamp_arduino/Lamp_arduino.py
+++ Lamp_arduino/Lamp_arduino.py
@@ -1,6 +1,7 @@
+import bluetooth
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtBluetooth import QBluetoothDeviceInfo, QBluetoothDeviceDiscoveryAgent, QBluetoothLocalDevice
-from PyQt5.QtCore import QIODevice, pyqtSlot, QCoreApplication, QTimer, QEventLoop
+from PyQt5.QtBluetooth import QBluetoothDeviceDiscoveryAgent
+from PyQt5.QtCore import QTimer, QEventLoop
 
 app = QtWidgets.QApplication([])
 ui = uic.loadUi("des.ui")
@@ -9,7 +10,7 @@ ui.setWindowTitle("Test_Lamp")
 list_device = []
 
 def startDeviceDiscovery():
-    # Create a discovery agent and connect to its signals
+    #Create a discovery agent and connect to its signals
     discoveryAgent = QBluetoothDeviceDiscoveryAgent()
     discoveryAgent.deviceDiscovered.connect(deviceDiscover)
 
@@ -22,17 +23,38 @@ def startDeviceDiscovery():
     # Set up a timer to exit the event loop after a certain time
     timer = QTimer()
     timer.timeout.connect(loop.quit)
-    timer.start(3000)  # 10 seconds
+    timer.start(1000)  # 10 seconds
 
     loop.exec_()
+
 def deviceDiscover(device):
-    print(f"Found device: {device.name()} ({device.address().toString()})")
-    find_device=device.name()+" "+device.address().toString()
+    find_device=device.name()+";"+device.address().toString()
     list_device.append(find_device)
 
+def onOpen():
+    address = ui.BluetoothBox.currentText()
+    target_address = address.split(';')
+    port = 1  # Зазвичай, порт 1 використовується для SPP (Serial Port Profile)
+
+    try:
+        socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        socket.connect((target_address[1], port))
+        print("Connected to device:", target_address[1])
+
+        #socket.close()
+    except bluetooth.BluetoothError as e:
+        print("Connection failed:", str(e))
+
+
+def onClose():
+    socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    socket.close()
+
+
 startDeviceDiscovery()
-print(list_device)
 ui.BluetoothBox.addItems(list_device)
+ui.OpenButton.clicked.connect(onOpen)
+ui.CloseButton.clicked.connect(onClose)
 
 ui.show()
 app.exec()
