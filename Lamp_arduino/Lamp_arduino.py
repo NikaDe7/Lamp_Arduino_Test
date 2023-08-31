3630f32a708c4278af7161703d43d479cdc9591e
Tanya <vorobest5@gmail.com>
2023-09-01 02:02:46 +0300

    On and Off lamp, also disconnect

--- Lamp_arduino/Lamp_arduino.py
+++ Lamp_arduino/Lamp_arduino.py
@@ -8,7 +8,7 @@ ui = uic.loadUi("des.ui")
 ui.setWindowTitle("Test_Lamp")
 
 list_device = []
-
+socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 def startDeviceDiscovery():
     #Create a discovery agent and connect to its signals
     discoveryAgent = QBluetoothDeviceDiscoveryAgent()
@@ -37,24 +37,30 @@ def onOpen():
     port = 1  # Зазвичай, порт 1 використовується для SPP (Serial Port Profile)
 
     try:
-        socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
         socket.connect((target_address[1], port))
         print("Connected to device:", target_address[1])
-
-        #socket.close()
     except bluetooth.BluetoothError as e:
         print("Connection failed:", str(e))
 
 
 def onClose():
-    socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     socket.close()
 
+def ON():
+    data_to_send = b'1'
+    socket.send(data_to_send)
+
+def OFF():
+    data_to_send = b'0'
+    socket.send(data_to_send)
 
 startDeviceDiscovery()
 ui.BluetoothBox.addItems(list_device)
 ui.OpenButton.clicked.connect(onOpen)
 ui.CloseButton.clicked.connect(onClose)
+
+ui.OnButton.clicked.connect(ON)
+ui.OffButton.clicked.connect(OFF)
 
 ui.show()
 app.exec()
